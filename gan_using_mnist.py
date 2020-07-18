248a16c13344299e3fff09a2ffde24ee0be49cd3
BibhuLamichhane <lamichhanebibhu0@gmail.com>
2020-07-18 20:16:08 +0545

    applying pep8 to the code

--- gan_using_mnist.py
+++ gan_using_mnist.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from tensorflow.keras.layers import Input, Dense, LeakyReLU, Dropout, BatchNormalization as bn
+from tensorflow.keras.layers import Input, Dense, LeakyReLU, Dropout, BatchNormalization
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam as adam, SGD
+from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.datasets import mnist
 
 data = mnist.load_data()
@@ -21,49 +21,49 @@ x_test = x_test.reshape(10000, 784)
 latent_dim = 200
 
 
-def training_test_data(n):
-    new_x_train = []
+def training_test_data(num):
+    n_x_train = []
     for i in range(len(y_train)):
-        if y_train[i] == n:
-            new_x_train.append(x_train[i])
+        if y_train[i] == num:
+            n_x_train.append(x_train[i])
 
-    new_x_train = np.array(new_x_train)
-    new_y_train = np.zeros(len(new_x_train))
-    new_y_train[:] = n
+    n_x_train = np.array(n_x_train)
+    n_y_train = np.zeros(len(n_x_train))
+    n_y_train[:] = num
 
-    new_x_test = []
+    n_x_test = []
     for i in range(len(y_test)):
-        if y_test[i] == n:
-            new_x_test.append(x_test[i])
+        if y_test[i] == num:
+            n_x_test.append(x_test[i])
 
-    new_x_test = np.array(new_x_test)
-    new_y_test = np.zeros(len(new_x_test))
-    new_y_test[:] = n
+    n_x_test = np.array(n_x_test)
+    n_y_test = np.zeros(len(n_x_test))
+    n_y_test[:] = num
 
-    return new_x_train, new_y_train, new_x_test, new_y_test
+    return n_x_train, n_y_train, n_x_test, n_y_test
 
 
-def gen(latent_dim):
-    i = Input(shape=latent_dim)
-    x = Dense(256, activation=LeakyReLU(alpha=0.2))(i)
-    x = bn(momentum=0.8)(x)
-    x = Dense(256, activation=LeakyReLU(alpha=0.2))(x)
-    x = bn(momentum=0.8)(x)
-    x = Dense(256, activation=LeakyReLU(alpha=0.2))(x)
-    x = bn(momentum=0.8)(x)
-    x = Dense(784, activation='tanh')(x)
+def gen(latent_dims):
+    i = Input(shape=latent_dims)
+    layer = Dense(256, activation=LeakyReLU(alpha=0.2))(i)
+    layer = BatchNormalization(momentum=0.8)(layer)
+    layer = Dense(256, activation=LeakyReLU(alpha=0.2))(layer)
+    layer = BatchNormalization(momentum=0.8)(layer)
+    layer = Dense(256, activation=LeakyReLU(alpha=0.2))(layer)
+    layer = BatchNormalization(momentum=0.8)(layer)
+    layer = Dense(784, activation='tanh')(layer)
 
-    model = Model(i, x)
+    model = Model(i, layer)
     return model
 
 
 def discriminator(size):
     i = Input(shape=(size,))
-    x = Dense(512, activation=LeakyReLU(alpha=0.2))(i)
-    x = Dense(256, activation=LeakyReLU(alpha=0.2))(x)
-    x = Dense(1, activation='sigmoid')(x)
+    layer = Dense(512, activation=LeakyReLU(alpha=0.2))(i)
+    layer = Dense(256, activation=LeakyReLU(alpha=0.2))(layer)
+    layer = Dense(1, activation='sigmoid')(layer)
 
-    model = Model(i, x)
+    model = Model(i, layer)
 
     return model
 
@@ -72,7 +72,7 @@ Discriminator = discriminator(784)
 
 Discriminator.compile(
     loss='binary_crossentropy',
-    optimizer=adam(0.0002, 0.5),
+    optimizer=Adam(0.0002, 0.5),
     metrics=['accuracy']
 )
 
@@ -86,7 +86,7 @@ fake_img = Discriminator(img)
 main_model = Model(x, fake_img)
 main_model.compile(
     loss='binary_crossentropy',
-    optimizer=adam(0.0002, 0.5),
+    optimizer=Adam(0.0002, 0.5),
 )
 
 batch = 32
@@ -103,21 +103,21 @@ if not os.path.exists('gan_images'):
     os.makedirs('gan_images')
 
 
-def sample_images(epoch, n):
+def sample_images(iteration, num):
     rows, cols = 5, 5
-    noise = np.random.randn(rows * cols, latent_dim)
-    imgs = generator.predict(noise)
+    noises = np.random.randn(rows * cols, latent_dim)
+    imgs = generator.predict(noises)
 
     imgs = 0.5 * imgs + 0.5
 
     fig, axs = plt.subplots(rows, cols)
-    idx = 0
+    id = 0
     for i in range(rows):
         for j in range(cols):
-            axs[i, j].imshow(imgs[idx].reshape(28, 28), cmap='gray')
+            axs[i, j].imshow(imgs[id].reshape(28, 28), cmap='gray')
             axs[i, j].axis('off')
-            idx += 1
-    fig.savefig(f"gan_images/{n}--{epoch}.png")
+            id += 1
+    fig.savefig(f"gan_images/{num}--{iteration}.png")
     plt.close()
 
 
@@ -136,9 +136,6 @@ for n in range(10):
         d_loss_fake, d_acc_fake = Discriminator.train_on_batch(fake_imgs, zeros)
         d_loss = 0.5 * (d_loss_real + d_loss_fake)
         d_acc = 0.5 * (d_acc_real + d_acc_fake)
-
-        noise = np.random.randn(batch, latent_dim)
-        g_loss = main_model.train_on_batch(noise, ones)
 
         noise = np.random.randn(batch, latent_dim)
         g_loss = main_model.train_on_batch(noise, ones)
